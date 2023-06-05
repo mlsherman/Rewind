fafa37b4e7792d28d162e7595b13921886cf358c
mlsherman <advancedautosec@yahoo.com>
2023-06-05 10:48:57 -0400

    finished up

--- js/site.js
+++ js/site.js
@@ -1,4 +1,45 @@
-// Call Hello World 
-function HelloWorld() {
-    alert("Hello World")
+// Get the string from the page
+// controller function
+function getValues(){
+
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+}
+
+// Reverse the string
+// logic function
+function reverseString(userString){
+
+    let revString = [];
+
+    // let name = Bobby;
+    // name[0] = B;
+    // name[4] = y;
+
+    // last position in an array name.length - 1
+
+    // reverse a string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+       revString += userString[index];
+    }
+
+    return revString
+
+}
+
+// Display the reversed string to the user
+// view function
+function displayString(revstring){
+
+    // write to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revstring}`;
+    // show the alert box
+    document.getElementById("alert").classList.remove("invisible");
+
+
 }
